5af3bc606d1dccda06260002ac42346b4437212f
wank125 <1143430962@qq.com>
2017-09-21 15:45:01 +0800

    修改

--- boy_name.py
+++ boy_name.py
@@ -5,13 +5,13 @@ import itchat
 from   pandas import DataFrame,Series
 itchat.login()
 friends=itchat.get_friends(update=True)
-file_frd=open('c:/workspace/frd.txt','w')
+file_frd=open('./workspace/frd.txt','w')
 #file_frd.write(friends)
 #file_frd.write('eew')
 
 
-names1880=pd.read_csv('C:\workspace\pydata-book-master/ch02/names/yob1880.txt',names=['name','sex','births'])
-print names1880
+#names1880=pd.read_csv('C:\workspace\pydata-book-master/ch02/names/yob1880.txt',names=['name','sex','births'])
+#print names1880
 print friends
 city_a=[ct['City'] for ct in friends if'City' in ct ]
 file_frd.write(city_a)
