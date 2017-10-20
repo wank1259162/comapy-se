87a98cba36862eddfb1db1edf0ec8519ee526a94
wank125 <1143430962@qq.com>
2017-10-20 16:50:15 +0800

    xiug

--- workspace/PelicanStores.py
+++ workspace/PelicanStores.py
@@ -9,5 +9,21 @@ import matplotlib.pyplot as plt
 inputfile = 'fil.csv'
 
 data_peli=pd.read_csv(inputfile) #导入数据
-data_peli.plot.scatter(x=data_peli.iloc[:,4], y=data_peli.iloc[:,8])
+#data_peli.plot.scatter(x=data_peli.iloc[:,4], y=data_peli.iloc[:,8])
+#data_peli.plot.scatter(x=data_peli.iloc[:,4], y=data_peli.iloc[:,8])
+fig=plt.figure(figsize=(10,7))
+#ax1=fig.add_subplot(2,2,1,title='TS1')
+#ax1.set_xlabel('Time')
+#ts1.plot(ax=ax1,label='TS1',legend=True)
+#净销售额和顾客年龄间的散点图
+plt.scatter(data_peli[u'Age'],data_peli[u'Net Sales'])
+
 plt.show()
+
+#plt.box(data_peli[u'Net Sales'])
+plt.ylabel("ARI") 
+plt.xlabel("Dissimilarity Measures") 
+data_peli[u'Net Sales'].plot.box()
+plt.show()
+data_peli.plot.box()
+
